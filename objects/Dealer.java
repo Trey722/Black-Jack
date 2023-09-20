007ac5ae01ef706b8406e79fb920b49672681ea1
RougeAce <127271451+RougeAce@users.noreply.github.com>
2023-09-19 22:05:13 -0500

    Added Player

--- objects/Dealer.java
+++ objects/Dealer.java
@@ -1,21 +1,42 @@
 package objects;
 
 public class Dealer {
-    Card cardUp; 
-    Card cardDown; 
+    hand Hand; 
     deck Deck; 
 
 
     public Dealer(deck Decks) {
-        Card cardUp = null; 
-        Card cardDown = null; 
+        
         deck Deck = Decks; 
     }
 
-    public void Deal(Player[] players) {
-        for (int i = 0; i < players; i++)
-        {
+    public void DealStart(Player player) {
 
+       player.Hand.cards[0] = Deck.deal(); 
+       player.Hand.cards[1] = Deck.deal(); 
+       player.Hand.size = 2; 
+
+       Hand.cards[0] = Deck.deal(); 
+       Hand.cards[1] = Deck.deal(); 
+
+       Hand.size = 2; 
+
+    }
+
+    public int DealUntilWin(int value) //Returns 1 on win | Returns 0 in Loss
+    {
+        while (Utility.calculateDeckValue(Hand) < value)
+        {
+            int size = Hand.size; 
+            Hand.cards[size] = Deck.deal(); 
+            Hand.size += 1; 
         }
+
+        if (Utility.calculateDeckValue(Hand) > 21)
+        {
+            return 0; 
+        }
+
+        return 1; 
     }
 }
